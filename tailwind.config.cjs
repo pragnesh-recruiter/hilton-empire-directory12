0b698fee1516607e0e3f4abebb163b02aae5b443
pragnesh-recruiter <84995764+pragnesh-recruiter@users.noreply.github.com>
2025-10-19 13:05:07 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#D4AF37',
+        pagebg: '#000000'
+      }
+    }
+  },
+  plugins: [],
+}
